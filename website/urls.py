b75377f562872b331a1272f26d8aae8820b5f9e0
Michał Wrona <wronamichal01@gmail.com>
2022-04-10 20:57:45 +0200

    Update urls (static) for Azure Deploy v2

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('', views.main_page, name="main_page"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
