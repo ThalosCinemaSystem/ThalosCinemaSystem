e881b2ff9d6ea9a1cb8e1c0245ebf7a89fc4d1ed
Michał Wrona <wronamichal01@gmail.com>
2022-04-18 22:48:44 +0200

    Added login and register

---
+++ authentication/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from .backends import UserLoginBackend
+from django.contrib.auth import login as auth_login
+
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = UserLoginBackend().authenticate(request, email=email, password=password)
+        if user:
+            auth_login(request, user)
+            return redirect('home')
+
+    return render(request, 'authentication/login.html', {})
+
+
+def register(request):
+    form = UserRegisterForm()
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('home')
+
+    context = {'form': form}
+
+    return render(request, 'authentication/register.html', context=context)
