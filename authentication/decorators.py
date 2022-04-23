1f8f73e8dd7fdeb97ede0bbd66ff83f282d6167e
karol <167861@stud.prz.edu.pl>
2022-04-23 16:51:03 +0200

    Added decorators

---
+++ authentication/decorators.py
@@ -0,0 +1,12 @@
+from django.shortcuts import redirect
+
+
+def unauthenticated(url=''):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+            if request.user.is_authenticated:
+                return redirect(url)
+            else:
+                return view_func(request, *args, **kwargs)
+        return wrapper_func
+    return decorator
