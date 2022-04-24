7bb07c745c06129e833096b7efa0e78d89f1cec1
KacperUrban <urbankacper613@gmail.com>
2022-04-24 19:40:01 +0200

    Create urls and views for each sub-site

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('account/', views.account, name="account"),
+    path('events_and_promotions/', views.events_and_promotions, name="events_and_promotions"),
+    path('marathons/', views.marathons, name="marathons"),
+    path('repertoire/', views.repertoire, name="repertoire"),
+    path('schools/', views.schools, name="schools"),
 ]
--- website/views.py
+++ website/views.py
@@ -6,3 +6,28 @@ def home(request):
     context = {}
 
     return render(request, 'website/main.html', context=context)
+
+def account(request):
+    context = {}
+
+    return render(request, 'website/account.html', context=context)
+
+def events_and_promotions(request):
+    context = {}
+
+    return render(request, 'website/events_and_promotions.html', context=context)
+
+def marathons(request):
+    context = {}
+
+    return render(request, 'website/marathons.html', context=context)
+
+def repertoire(request):
+    context = {}
+
+    return render(request, 'website/repertoire.html', context=context)
+
+def schools(request):
+    context = {}
+
+    return render(request, 'website/schools.html', context=context)
