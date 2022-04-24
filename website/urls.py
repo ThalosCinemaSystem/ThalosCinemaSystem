7d218560101186fb48464b62d811f5f940011d6b
Pawel Wladyka <wladykapagmail.com>
2022-04-24 12:17:49 +0200

    maratons project beta

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name="main_page"),
+    path('maratony', views.maratons, name="maratons"),
+    path('szkoly', views.schools, name="schools"),
 ]
--- website/views.py
+++ website/views.py
@@ -5,3 +5,13 @@ def main_page(request):
     context = {}
 
     return render(request, 'website/main.html', context=context)
+
+def maratons(request):
+    context = {}
+
+    return render(request, 'website/maratons.html', context=context)
+
+def schools(request):
+    context = {}
+
+    return render(request, 'website/schools.html', context=context)
