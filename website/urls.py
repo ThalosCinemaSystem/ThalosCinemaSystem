bab136b2b1a807b756f5ca82ff3f6b263814f4fc
karol <167861@stud.prz.edu.pl>
2022-05-14 17:42:00 +0200

    - Add logout - Add change password

--- website/urls.py
+++ website/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name="main_page"),
+    path('logout', views.logoutUser, name="logout"),
     path('marathons', views.marathons, name="marathons"),
     path('schools', views.schools, name="schools"),
     path('account', views.account, name="account"),
     path('events_and_promotions', views.events_and_promotions, name="events_and_promotions"),
     path('repertoire', views.repertoire, name="repertoire"),
+    path('change-password', views.change_password, name='change_password'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 from django.template import RequestContext
-
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 def main_page(request):
     context = {}
@@ -46,3 +48,23 @@ def error_404(request, exception):
 def error_500(request):
     data = {"error_http": "Błąd 500"}
     return render(request, 'website/error_http.html', context=data)
+
+def logoutUser(request):
+    logout(request)
+    return redirect(main_page)
+
+def change_password(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PasswordChangeForm(request.user, request.POST)
+            if form.is_valid():
+                user = form.save()
+                update_session_auth_hash(request, user)
+                return redirect('change_password')
+        else:
+            form = PasswordChangeForm(request.user)
+        return render(request, 'website/change_password.html', {
+            'form': form
+        })
+    else:
+        return redirect(main_page)
