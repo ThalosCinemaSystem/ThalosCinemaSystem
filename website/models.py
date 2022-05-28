1c1f3c76ab4441ba7e52095a778f0b49a3ef08cf
karol <167861@stud.prz.edu.pl>
2022-05-28 20:17:22 +0200

    Fix backend in main

--- website/models.py
+++ website/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from azure.storage.blob import BlobClient
 import os
+from datetime import timedelta
 
 try:
     os.environ["IS_PRODUCTION"]
@@ -10,6 +11,7 @@ except KeyError:
 else:
     from TCS.settings import AZURE_CUSTOM_DOMAIN, AZURE_ACCOUNT_NAME
     from TCS.custom_azure import ACCOUNT_KEY
+
 
     def get_blob_service(container_name, blob_name):
         account_url = AZURE_CUSTOM_DOMAIN
@@ -21,22 +23,31 @@ else:
 
 
 class Cinema(models.Model):
-    city = models.CharField(max_length=30, null=True)
-    street = models.CharField(max_length=30, null=True)
-    country = models.CharField(max_length=30, null=True)
-    name = models.CharField(max_length=30, null=True)
-    address = models.CharField(max_length=30, null=True)
+    city = models.CharField(max_length=255, null=True)
+    street = models.CharField(max_length=255, null=True)
+    country = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=255, null=True)
+    address = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+
+    name = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.name
 
 
+
 class Movie(models.Model):
-    title = models.CharField(max_length=30, null=True)
+    title = models.CharField(max_length=255, null=True)
     description = models.CharField(max_length=1024, null=True)
     during = models.IntegerField(null=True)
     thumbnail = models.ImageField(upload_to='movies_images')
-    url_trailer = models.CharField(max_length=30, null=True)
+    url_trailer = models.CharField(max_length=255, null=True)
+    genre = models.ManyToManyField(Genre)
 
     def __str__(self):
         return self.title
@@ -51,28 +62,26 @@ class Movie(models.Model):
             blob_service.delete_blob()
             super(Movie, self).delete(*args, **kwargs)
 
-class Genre(models.Model):
-    names = (
-        ('Dramat', 'Dramat'),
-        ('Horror', 'Horror'),
-        ('Akcji', 'Akcji'),
-        ('Komedia', 'Komedia'),
-        ('Romantyczny', 'Romantyczny'),
-        ('Wojenny', 'Wojenny'),
-        ('Kryminalny', 'Kryminalny'),
-        ('Fantasy', 'Fantasy'),
-        ('Sci-Fi', 'Sci-Fi'),
-        ('Thriller', 'Thriller'),
-
-    )
-    name = models.CharField(max_length=20, null=True, choices=names)
-    movie = models.ForeignKey(Movie, null=True, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.name
 
 class Room(models.Model):
     number = models.IntegerField(null=True)
     cinema = models.ForeignKey(Cinema, null=True, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not Seat.objects.filter(room=self):
+            super(Room, self).save(*args, **kwargs)
+            seat_objs = [Seat(seat_number=seat_number,
+                              number_of_row=None,
+                              number_of_column=None,
+                              room=self)
+                         for seat_number in range(0, 100)]
+
+            Seat.objects.bulk_create(seat_objs)
+
+    def delete(self, *args, **kwargs):
+        seats = Seat.objects.filter(room=self).all()
+        seats.delete()
+        super(Room, self).delete(*args, **kwargs)
 
     def __str__(self):
         return str(self.number)
@@ -83,9 +92,36 @@ class Seat(models.Model):
     number_of_column = models.IntegerField(null=True)
     number_of_row = models.IntegerField(null=True)
     room = models.ForeignKey(Room, null=True, on_delete=models.CASCADE)
+    is_durning_reservation = models.BooleanField(default=False)
 
     def __str__(self):
-        return str(self.seat_number)
+        return str(f'Seat Number: {self.seat_number} - Room Number: {self.room}')
+
+    @property
+    def is_reservated(self):
+        if Reservation.objects.filter(seat_id=self).first():
+            return True
+        return False
+
+
+class Marathon(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    description = models.CharField(max_length=1024, null=True)
+    price = models.FloatField(null=True)
+    thumbnail = models.ImageField(null=True, blank=True)
+
+    try:
+        os.environ["IS_PRODUCTION"]
+    except KeyError:
+        pass
+    else:
+        def delete(self, *args, **kwargs):
+            blob_service = get_blob_service(container_name='media', blob_name=self.thumbnail.name)
+            blob_service.delete_blob()
+            super(Marathon, self).delete(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Projection(models.Model):
@@ -100,9 +136,26 @@ class Projection(models.Model):
     price = models.FloatField(null=True)
     movie = models.ForeignKey(Movie, null=True, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, null=True, on_delete=models.CASCADE)
+    marathon = models.ForeignKey(Marathon, blank=True, null=True, on_delete=models.CASCADE)
+    is_cyclic = models.BooleanField(default=False, null=True)
 
     def __str__(self):
-        return self.movie.title
+        return f'{self.movie.title} - {self.start_date_time} - Room: {self.room.number} - IS_CYCLIC: {self.is_cyclic}'
+
+    def save(self, *args, **kwargs):
+        if self.is_cyclic:
+            projections = [Projection(start_date_time=self.start_date_time + timedelta(weeks=i),
+                                      is_3d=self.is_3d,
+                                      type_projection=self.type_projection,
+                                      price=self.price,
+                                      movie=self.movie,
+                                      room=self.room,
+                                      marathon=self.marathon,
+                                      is_cyclic=self.is_cyclic)
+                           for i in range(0, 10)]
+            Projection.objects.bulk_create(projections)
+        else:
+            super(Projection, self).save(*args, **kwargs)
 
 
 class Reservation(models.Model):
@@ -112,10 +165,3 @@ class Reservation(models.Model):
 
     def __str__(self):
         return self.user.get_username()
-
-
-class Marathon(models.Model):
-    name = models.CharField(max_length=30, null=True)
-    description = models.CharField(max_length=1024, null=True)
-    price = models.FloatField(null=True)
-    thumbnail = models.ImageField(null=True, blank=True)
--- website/views.py
+++ website/views.py
@@ -1,21 +1,20 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
-from django.template import RequestContext
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
-from django.utils.dateparse import parse_datetime
 from .models import *
 import datetime
+from django.contrib.auth.decorators import login_required
 
 
-def main_page(request, pk=None, pk2=None, pk3='ALL', pk4=1):
-    pk = Cinema.objects.first()
-    pk2 = datetime.date.today().strftime('%Y-%m-%d')
+def main_page(request, pk=Cinema.objects.first(), pk2=None, pk3=None, pk4=1):
+    if pk2 is None:
+        pk2 = datetime.date.today().strftime('%Y-%m-%d')
 
     projection = Projection.objects.filter(room__cinema__name=pk)
     projection = projection.filter(start_date_time__contains=pk2)
-    if pk3 != 'ALL':
-        projection = projection.filter(movie__title__contains=Genre.objects.filter(name=pk3).values('movie_id__title'))
+    if pk3 is not None and pk3 != 'None':
+        projection = projection.filter(movie_id__genre__name=pk3)
     weekend = ["NIEDZ", "PON", "WT", "ŚR", "CZW", "PT", "SOB"]
     Date = {}
     Date[0] = {}
@@ -36,6 +35,7 @@ def main_page(request, pk=None, pk2=None, pk3='ALL', pk4=1):
             projections[i.movie.title]['Thumbnail'] = i.movie.thumbnail.url
             projections[i.movie.title]['start_date_time'] = []
             projections[i.movie.title]['Genre'] = Genre.objects.filter(movie__title=i.movie.title)
+            projections[i.movie.title]['projection_movie_id'] = i.movie.id
             id = id + 1
 
         projections[i.movie.title]['start_date_time'].append(i.start_date_time)
@@ -55,13 +55,19 @@ def main_page(request, pk=None, pk2=None, pk3='ALL', pk4=1):
         if y['id'] not in range(pk4 * 5 - 5, pk4 * 5):
             del projections[x]
     cinemas = Cinema.objects.all()
+    genres = Genre.objects.all()
+    releases = Projection.objects.filter(start_date_time__gte=datetime.date.today() + datetime.timedelta(days=7)).values('movie_id','movie_id__title','movie_id__thumbnail', 'movie_id__description').distinct()[:3]
     context = {'cinemas': cinemas, 'projections': projections, "Date": Date, "pk": pk, 'pk2': pk2, 'pk3': pk3,
-               'pk4': pk4, 'pages': pages}
+               'pk4': pk4, 'pages': pages, 'releases':releases,'genres':genres}
     return render(request, 'website/main.html', context=context)
 
 
 def marathons(request):
-    context = {}
+    marathon = Marathon.objects.first()
+    projections = Projection.objects.filter(marathon=marathon).all()
+
+    context = {'marathon': marathon,
+               'projections': projections}
 
     return render(request, 'website/marathons.html', context=context)
 
@@ -100,11 +106,13 @@ def error_500(request):
     return render(request, 'website/error_http.html', context=data)
 
 
-def logoutUser(request):
+@login_required(login_url='main_page')
+def logout_user(request):
     logout(request)
     return redirect(main_page)
 
 
+@login_required(login_url='main_page')
 def change_password(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
@@ -120,3 +128,69 @@ def change_password(request):
         })
     else:
         return redirect(main_page)
+
+
+def book_movie(request, movie_pk, room=None, hour=None):
+    projections = Projection.objects.filter(movie_id=movie_pk)
+
+    context = {'projections': projections}
+
+    return render(request, 'website/projection_to_reservation.html', context=context)
+
+
+@login_required(login_url='main_page')
+def book_movie_projection(request, movie_pk, projection_pk):
+    seats = Seat.objects.filter(room__projection=projection_pk)
+    divided_seats = [seats[i:i + 10] for i in range(0, 100) if i % 10 == 0]
+
+    if request.method == 'POST':
+        seat_pk = request.POST.get('seat')
+        seat_obj = Seat.objects.get(pk=seat_pk)
+        if not seat_obj.is_reservated:
+            seat_obj.is_durning_reservation = True
+            seat_obj.save()
+            request.session['projection_id_reserving'] = projection_pk
+            request.session['seat_id_reserving'] = seat_obj.pk
+
+            return redirect('reservation_summary')
+
+    if request.session.get('projection_id_reserving', False) and request.session.get('seat_id_reserving', False):
+        return redirect('reservation_summary')
+
+    context = {'divided_seats': divided_seats}
+    return render(request, 'website/choice_seat.html', context=context)
+
+
+@login_required(login_url='main_page')
+def reservation_summary(request):
+    projection_id = request.session.get('projection_id_reserving', False)
+    seat_id = request.session.get('seat_id_reserving', False)
+
+    if projection_id and seat_id:
+        projection = Projection.objects.get(pk=projection_id)
+        seat = Seat.objects.get(pk=seat_id)
+
+        if request.GET.get('reservation') == 'done':
+            reservation = Reservation(user=request.user, seat=seat, projection=projection)
+            reservation.save()
+            del request.session['projection_id_reserving']
+            del request.session['seat_id_reserving']
+            seat.is_durning_reservation = False
+            seat.save()
+
+            return redirect('main_page')
+
+        elif request.GET.get('reservation') == 'interrupt':
+            del request.session['projection_id_reserving']
+            del request.session['seat_id_reserving']
+            seat.is_durning_reservation = False
+            seat.save()
+
+            return redirect('main_page')
+
+        context = {'projection': projection,
+                   'seat': seat}
+
+        return render(request, 'website/summary_reservation.html', context=context)
+
+    return redirect(main_page)
