7f617e59ed5a5722da70262446df695711e0ab9f
Michał Wrona <wronamichal01@gmail.com>
2022-09-10 18:57:17 +0200

    Update docker and django

--- TCS/settings.py
+++ TCS/settings.py
@@ -151,11 +151,12 @@ CSRF_TRUSTED_ORIGINS = ['http://0.0.0.0:8000', 'https://thalos.software']
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'media',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = BASE_DIR / 'mediafiles/'
 MEDIA_URL = 'media/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
