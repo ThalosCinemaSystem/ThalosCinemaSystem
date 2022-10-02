51dc4fb1219d3cbb11475f1f2990d745cb50886e
Michał Wrona <wronamichal01@gmail.com>
2022-10-02 19:24:02 +0200

    Update

--- TCS/settings.py
+++ TCS/settings.py
@@ -153,10 +153,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = BASE_DIR / 'mediafiles/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = 'media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
