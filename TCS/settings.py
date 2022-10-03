ce5ec9cce32254db7d286bf54c092d1af72665d1
Michał Wrona <wronamichal01@gmail.com>
2022-10-03 23:56:19 +0200

    Update debug

--- TCS/settings.py
+++ TCS/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', 'False')
+DEBUG = os.environ.get('DEBUG')
 
 DATABASES = {
     'default': {
