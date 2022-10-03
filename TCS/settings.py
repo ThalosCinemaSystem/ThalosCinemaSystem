c781624060aca9bd5cdc362164a16d81217a3f0f
Michał Wrona <wronamichal01@gmail.com>
2022-10-03 23:50:12 +0200

    Update for prod and dev environs

--- TCS/settings.py
+++ TCS/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ.get('DEBUG', 'False')
 
 DATABASES = {
     'default': {
