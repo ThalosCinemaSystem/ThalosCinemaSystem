309e569d25c8bd981500eeababf25b87b7c1806a
Michał Wrona <wronamichal01@gmail.com>
2022-10-10 22:48:20 +0200

    Fix date v2

--- website/views.py
+++ website/views.py
@@ -62,7 +62,7 @@ def main_page(request, pk=None, pk2=None, pk3=None, pk4=1):
     P_Calendar[1] = P_Calendar[1].formatmonth(withyear=True)
     projection = Projection.objects.filter(room__cinema__name=pk)
     pk2_splitted = pk2.split('-')
-    pk2 = f"{'-'.join(pk2_splitted[:2])}-0{pk2_splitted[2]}" if 0 < int(pk2_splitted[2]) < 10 else pk2
+    pk2 = datetime.datetime.strptime(pk2, '%Y-%m-%d').date().__str__()
     projection = projection.filter(start_date_time__contains=pk2)
     if pk3 is not None and pk3 != 'None':
         projection = projection.filter(movie_id__genre__name=pk3)
