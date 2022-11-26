a5431f3fde2aeb1a7ff274be3e92f6f14724af67
Michał Wrona <wronamichal01@gmail.com>
2022-11-26 22:10:04 +0100

    Update health-check to return HTTP status code 200

--- website/views.py
+++ website/views.py
@@ -249,7 +249,6 @@ def reservation_summary(request):
     return redirect(main_page)
 
 def health_check(request):
-    response = HttpResponse(content='Health Check: OK')
+    response = HttpResponse(content='Health Check: OK', status=200)
     response.headers['Cache-Control'] = 'no-store'
-    response.status_code = 200
     return response
